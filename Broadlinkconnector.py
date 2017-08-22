1b99899b7716ed9e31b5736a1a76014d1dad05e9
joplotki <isrplo@gmail.com>
2017-08-22 22:32:02 +0300

    first push

---
+++ Broadlinkconnector.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import logging
+import socket
+import time
+
+import yaml
+
+import zauto.broadlink
+
+
+class Broadlinkconnector:
+    __instance = None
+
+    def __init__(self):
+        if Broadlinkconnector.__instance is not None:
+            return
+        devices = {}
+        gotActictiveConnections = False
+        Broadlinkconnector.__instance = self
+        self.init('config.yaml')
+
+
+    @staticmethod
+    def getInstance():
+        if Broadlinkconnector.__instance is None:
+            Broadlinkconnector()
+        return Broadlinkconnector.__instance
+
+    def setLogger(self,logger):
+        self.logger = logger
+
+    def init(self,configFileName):
+
+        logger = logging.getLogger(__name__)
+
+        with open(configFileName, 'r') as stream:
+            config = yaml.load(stream)
+        devices={}
+        gotActictiveConnections=False
+
+        for dev in config['devices'].keys():
+            name = dev
+            devices[name] = {}
+            devices[name]['isConnected'] = False
+            ip = config['devices'][dev].get('ip',None)
+            mac = bytearray.fromhex("b4430fffffff")#dev.get('mac',None)
+            devices[name]['connection'] = zauto.broadlink.rm(host=(ip, 80), mac=mac)
+            logger.info("Connecting to Broadlink: device {} on ip {}".format(name,ip))
+            devices[name]['remotes']=config['devices'][name]['remotes']
+            try:
+                devices[name]['connection'].auth()
+            except socket.timeout:
+                logger.error("Connection timeout")
+                continue
+
+            devices[name]['isConnected'] = True
+            gotActictiveConnections = True
+            time.sleep(1)
+            logger.info("Connected to "+name)
+
+        self.devices = devices
+        self.gotActictiveConnections = gotActictiveConnections
+        self.urls = config.get('urls',None)
+
+    def execute(self, device, remote, operation):
+        logger=logging.getLogger(__name__)
+        dev = self.devices.get(device,None)
+        if dev is None:
+            logger.error("device {} does not exist".format(device))
+            return
+        rem = dev['remotes'].get(remote,None)
+        if rem is None:
+            logger.error("remote {} does not exist in device {}".format(remote, device))
+            return
+        op_hex = rem['oplist'].get(operation,None)
+        if op_hex is None:
+            logger.error("operation {} does not exist in remote {} in device {}".format(operation, remote, device))
+            return
+
+        if not dev['isConnected']:
+            logger.error("Cant execute command, device {} is not connected".format(device))
+            return
+
+        dev['connection'].send_data(op_hex.decode('hex'))
+        logger.info("Command {} executes successfully".format(operation))
+
+    def gotactictiveconnections(self):
+        return self.gotActictiveConnections
---
+++ restapi.py
@@ -0,0 +1,38 @@
+import json
+import logging
+
+import zauto.web
+from zauto.Broadlinkconnector import Broadlinkconnector
+
+FORMAT = '%(asctime)-15s: %(name)-8s: %(levelname)s: %(message)s'
+formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+logging.basicConfig(level=logging.INFO, format=FORMAT)
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler('broadlinkConnector.log')
+handler.setLevel(logging.INFO)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+Broadlinkconnector.getInstance()
+#bc.init('config.yaml')
+#bc.execute('rm_main','shades','up')
+
+url=('(.*)', 'perform_op')
+
+app = zauto.web.application(url, globals())
+
+if __name__ == "__main__":
+    app.run()
+
+
+logger.info('Ready')
+
+class perform_op:
+
+    def GET(self,url):
+        data = json.loads(zauto.web.data())
+        device = data['device']
+        remote = data['remote']
+        operation = data['operation']
+        Broadlinkconnector.getInstance().execute(device,remote,operation)
+        return 'OK'
